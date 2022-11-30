cae869467dc1bd94ad0b5f179cbe1ad9d15f5803
qdtrl <quentindtouroul@gmail.com>
2022-11-30 12:23:36 +0100

    initial

---
+++ pkg/build_a_smart_contract_in_rust.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {any}
+*/
+export function initialize(): any;
+/**
+* @param {any} context
+* @param {string} address
+* @returns {any}
+*/
+export function set_click(context: any, address: string): any;
+/**
+* @param {any} context
+* @returns {any}
+*/
+export function get_contract_account(context: any): any;
